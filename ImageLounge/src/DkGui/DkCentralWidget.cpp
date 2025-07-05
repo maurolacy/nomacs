6ebadc9383b75fff50ef01520978b2833649151c
Mauro Lacy <mauro@lacy.com.es>
2025-07-05 17:09:16 +0200

    Fix: Don't segfault when no files / viewport

--- ImageLounge/src/DkGui/DkCentralWidget.cpp
+++ ImageLounge/src/DkGui/DkCentralWidget.cpp
@@ -1054,7 +1054,9 @@ void DkCentralWidget::showProgress(bool show, int time)
 
 void DkCentralWidget::startSlideshow(bool start) const
 {
-    getViewPort()->getController()->startSlideshow(start);
+    if (hasViewPort()) {
+        getViewPort()->getController()->startSlideshow(start);
+    }
 }
 
 void DkCentralWidget::startSlideshowWithFiles(const QStringList& files) const
